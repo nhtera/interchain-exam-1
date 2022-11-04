fc1d1afd6dfa2f983b9ba64492add0390e9f1f9f
Tien Nguyen <nguyentien8x@gmail.com>
2022-11-04 21:32:36 +0700

    Update default NextOperatorId in SystemInfo

--- x/tollroad/types/genesis.go
+++ x/tollroad/types/genesis.go
@@ -10,7 +10,9 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		SystemInfo:       SystemInfo{},
+		SystemInfo:       SystemInfo{
+			NextOperatorId: uint64(DefaultIndex),
+		},
 		RoadOperatorList: []RoadOperator{},
 		// this line is used by starport scaffolding # genesis/types/default
 		Params: DefaultParams(),
